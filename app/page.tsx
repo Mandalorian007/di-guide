a8aa1fd0e0a3e6f634d16140edcb3b9f204c3be3
matthew <mandalorian007@gmail.com>
2025-03-10 10:59:06 -0400

    Fix linting errors: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
-import { MoveUpRightIcon, TerminalSquareIcon, SwordIcon } from "lucide-react";
+import { MoveUpRightIcon, SwordIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
